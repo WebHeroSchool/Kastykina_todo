6f53f6197a2c164643283e943ca64258185bb9a6
Galina Kastykina <galina_kastykina@users.noreply.github.com>
2020-04-07 22:51:33 +0300

    some ch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { count, length } from './number.js';
+import { count, length } from './number';
 
 
 class App extends Component {
