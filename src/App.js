c0b4b2cb4365ed3d80cec2e7b03837c0ba62f607
Galina Kastykina <galina_kastykina@users.noreply.github.com>
2020-04-07 22:49:08 +0300

    lesson_modules

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-const variable = true;
-const name = 'Mary';
+import { count, length } from './number.js';
 
 
 class App extends Component {
@@ -20,33 +18,9 @@ class App extends Component {
           }}>
             Hello World!</p>
             <p>
-              {name}
+            count * length  =  {count * length}
             </p>
-            <p>
-              {15339}
-            </p>
-            <p>
-              {18*2}
-            </p>
-            <p>
-              {variable && 'Variable is true'}
-            </p>
-            <p>
-              Undefined: пусто {undefined}
-            </p>
-            <p>
-              Null: пусто {null}
-              </p>
-              <p>
-              False: пусто {false}
-              </p>
-              <p>
-                True: пусто {true}
-              </p>
-            <p>
-              {variable ? 'Everything is OK' : 'Variable is false'}
-            </p>
-
+            
           <a
             className="App-link"
             href="https://reactjs.org"
