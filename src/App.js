8c5aaf1887b32a7773e83a6e2a0a3c878e971026
Galina Kastykina <galina_kastykina@users.noreply.github.com>
2020-04-10 00:29:01 +0300

    react-element

--- src/App.js
+++ src/App.js
@@ -1,38 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { count, length } from './number';
+import React from 'react';
 
+const element = (<h1>
+  Мое первое React-приложение!
+  </h1>);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p style={{
-            fontStyle: 'italic'
-          }}>
-            Hello World!</p>
-            <p>
-            count * length  =  {count * length}
-            </p>
-            
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-
-export default App;
+  export {element};
